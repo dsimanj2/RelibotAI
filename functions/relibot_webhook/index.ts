d54ec36d9c3aab817cff38a5a479384bf53436a4
Denny <hariadi_denny@yahoo.com>
2025-07-23 10:59:47 +0200

    Update index.ts

--- functions/relibot_webhook/index.ts
+++ functions/relibot_webhook/index.ts
@@ -3,8 +3,10 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
 serve(async (req) => {
+  console.log("Webhook triggered");
+
   const body = await req.json();
-  console.log("Received:", body);
+  console.log("Received payload:", body);
 
   return new Response(JSON.stringify({ status: "received" }), {
     headers: { "Content-Type": "application/json" },
